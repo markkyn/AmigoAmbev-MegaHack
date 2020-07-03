ebcf99699edca359625fa42aa1f727acfe732453
Marcos Gabriel <marcos.gabriel@ossirian.com>
2020-07-03 08:31:40 -0300

    Add Base Styles

---
+++ src/components/AppContainer/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native';
+import { colors } from '../../styles';
+
+export const AppContainer = styled.SafeAreaView`
+    flex:1;
+    background-color: ${colors.background};
+`;
---
+++ src/styles/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    background:'#F1F1F1',
+    backgroundDark:'#EBE6E6',
+    primary :'#8d0b0b',
+    primaryDark :"#5E0C1C",
+    yellow: '#ffba08'
+};
+
+export default colors;
