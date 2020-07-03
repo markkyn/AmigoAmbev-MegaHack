0e95866f6498ddf47347f978b6763dcb930119ca
Marcos Gabriel <marcos.gabriel@ossirian.com>
2020-07-03 15:00:27 -0300

    Aba Cupons added

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import { View, Text } from 'react-native';
-import { Avatar } from 'react-native-elements';
+import { Avatar, ListItem } from 'react-native-elements';
 
-import { Body, TopView, AvatarView, DataView } from './styles';
+import { Body, TopView, AvatarView, DataView, MidView, CuponsView } from './styles';
+import { colors } from '../../styles';
 
 import { AppContainer } from '../../components/AppContainer'
 import Header from '../../components/Header'
@@ -11,6 +12,29 @@ import Header from '../../components/Header'
 import ambevAvatar from '../../assets/avatar.jpg';
 
 export default function Profile() {
+
+    const cupomArray = [
+        {
+            name: 'Retro Bar',
+            description: 'Olá',
+            avatar: 'https://www.logaster.com.br/blog/wp-content/uploads/sites/6/2019/03/t_Retro-Burger-Bar-Logo_1.png'
+        },
+        {
+            name: 'Tops Bar',
+            description: 'Olá',
+            avatar: 'https://i.pinimg.com/originals/62/d6/90/62d690a2857600e189a72963659bb9f6.jpg'
+        },
+        {
+            name: 'Bar do Juarez',
+            description: 'Olá',
+            avatar: 'https://static.wixstatic.com/media/7201a9_394287cd09314c90a77b61beeb484679~mv2_d_2466_2176_s_2.png/v1/fit/w_2500,h_1330,al_c/7201a9_394287cd09314c90a77b61beeb484679~mv2_d_2466_2176_s_2.png'
+        },
+        {
+            name: 'Lorem Ipsum Vinhos',
+            description: 'Olá',
+            avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHhYmNfeRYHLjKlRhMhPlgYkkXPYfZPvVY3g&usqp=CAU'
+        }
+    ]
     return (
         <AppContainer>
             <Header />
@@ -19,7 +43,7 @@ export default function Profile() {
 
                     <AvatarView>
                         <Avatar rounded size="xlarge" source={ambevAvatar} containerStyle={{ borderColor: '#262626', borderWidth: 3, marginBottom: 15 }} />
-                        <Text style={{ fontSize: 20 }}>Marcos Gabriel</Text>
+                        <Text style={{ fontSize: 20, color: colors.background }}>Marcos Gabriel</Text>
                         <Text style={{ color: '#999' }}>Aracaju , SE</Text>
                     </AvatarView>
 
@@ -28,45 +52,71 @@ export default function Profile() {
                             style={{ alignItems: 'center', width: 80 }}
                         >
                             <Text
-                                style={{ textAlign: "center", fontSize: 16, fontWeight: 'bold' }}
+                                style={{ textAlign: "center", fontSize: 16, fontWeight: 'bold', color: colors.background }}
                             >
                                 {`Bares\nVisitados`}
                             </Text>
                             <Text
-                                style={{ fontSize: 24 }}
+                                style={{ fontSize: 24, color: colors.background }}
 
-                            >4</Text>
+                            >
+                                4
+                            </Text>
 
                         </View>
                         <View
                             style={{ alignItems: 'center', flex: 1, width: 80 }}
                         >
                             <Text
-                                style={{ textAlign: "center", fontSize: 16, fontWeight: 'bold' }}
+                                style={{ textAlign: "center", fontSize: 16, fontWeight: 'bold', color: colors.background }}
                             >
                                 {`Ambev\nPoints`}
 
                             </Text>
                             <Text
-                                style={{ fontSize: 24 }}
-                            >5</Text>
+                                style={{ fontSize: 24, color: colors.background }}
+                            >
+                                5
+                            </Text>
 
                         </View>
                         <View
                             style={{ alignItems: 'center', width: 80 }}
                         >
                             <Text
-                                style={{ textAlign: "center", fontSize: 16, fontWeight: 'bold' }}
+                                style={{ textAlign: "center", fontSize: 16, fontWeight: 'bold', color: colors.background }}
                             >
                                 {`Cupons`}
                             </Text>
                             <Text
-                                style={{ fontSize: 24 }}
+                                style={{ fontSize: 24, color: colors.background }}
 
-                            >14</Text>
+                            >
+                                14
+                            </Text>
                         </View>
                     </DataView>
                 </TopView>
+                <MidView>
+                    <CuponsView>
+                        <Text style={{ alignSelf: 'center' }}>Cupons</Text>
+                        <View>
+
+                            {
+                                cupomArray.map((cupom, index) => {
+                                    return (
+                                        <ListItem
+                                            key={index}
+                                            leftAvatar={{ source: { uri: cupom.avatar } }}
+                                            title={cupom.name}
+                                            bottomDivider
+                                        />
+                                    )
+                                })
+                            }
+                        </View>
+                    </CuponsView>
+                </MidView>
             </Body>
         </AppContainer>
     );
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -2,14 +2,15 @@ import styled from 'styled-components/native';
 
 import { colors } from '../../styles';
 
-export const Body = styled.View`
+export const Body = styled.ScrollView`
     flex:1;
-    background-color: ${colors.background};
+    background-color: ${colors.primaryDark};
 `;
 
 export const TopView = styled.View`
     align-items: center;
-    margin-top: 30px;
+    padding-top: 30px;
+    border-bottom-color:#999;
 `;
 
 export const AvatarView = styled.View`
@@ -18,11 +19,25 @@ export const AvatarView = styled.View`
 `;
 export const DataView = styled.View`
     width: 100%;
-    padding: 0px 20px;
+    padding:70px 20px;
     flex-direction: row;
     align-items: center;
 `;
 
 export const Label = styled.Text`
+    color: ${colors.background};
+`;
+export const MidView = styled.View`
+    flex:1;
+    align-items:center;
+    background-color: ${colors.background};
+    border-top-right-radius: 30px;
+    border-top-left-radius: 30px;
 
+`;
+
+export const CuponsView = styled.View`
+    
+    width: 100%;
+    margin-top: 30px;
 `;
